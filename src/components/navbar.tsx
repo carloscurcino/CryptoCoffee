b29fa09e2eedbd6be29aa9d0932964e745eb9a57
carloscurcino <carlosdanielf541@gmail.com>
2024-03-18 18:29:57 -0300

    Feat: responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,9 @@
-import { BookOpenText, Mail, Mic, Newspaper, Settings, SquareArrowUpRight, Wallet, Youtube } from "lucide-react"
+import { BookOpenText, Mail, Menu, Mic, Newspaper, Settings, SquareArrowUpRight, Wallet, Youtube } from "lucide-react"
 import { Button } from "./ui/button"
 import { CurrencyEth } from "@phosphor-icons/react"
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuTrigger } from "./ui/navigation-menu"
 import { NavigationMenuList } from "@radix-ui/react-navigation-menu"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet"
 
 export const Navbar = () => {
     return (
@@ -11,57 +12,138 @@ export const Navbar = () => {
                 <img src="src/assets/cryptocoffee-logo.png" className="h-8 -mt-2" alt="" />
                 <p>CryptoCoffee</p>
             </div>
-            {/* <ul className="flex gap-16 items-center">
-                <li><a href="#" className="flex items-center text-center">Trade</a></li>
-                <li><a href="#" className="flex items-center text-center gap-2">DAO <ChevronDown /></a></li>
-                <li><a href="#" className="flex items-center text-center">Earn</a></li>
-                <li><a href="#" className="flex items-center justify-center gap-2">Bay Crypto <SquareArrowUpRight /></a></li>
-            </ul> */}
 
-            <NavigationMenu>
-                <NavigationMenuList className="flex gap-16 items-center">
-                    <NavigationMenuItem>
-                        <a href="#">
-                            <NavigationMenuLink>
-                                Trade
-                            </NavigationMenuLink>
-                        </a>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger className="bg-none">Lean</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="flex flex-col p-4 md:w-[100px] lg:w-[200px] gap-4">
-                                <li><a href="#" className="flex items-center text-center"><BookOpenText /> Learn Crypt</a></li>
-                                <li><a href="#" className="flex items-center text-center"><Newspaper /> News</a></li>
-                                <li><a href="#" className="flex items-center text-center"><Youtube /> Videos</a></li>
-                                <li><a href="#" className="flex items-center text-center"><Mic />Podcast</a></li>
-                                <li><a href="#" className="flex items-center text-center"><Mail /> Newsletter</a></li>
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <a href="#">
-                            <NavigationMenuLink className="flex items-center justify-center gap-2">
-                                Earn
-                            </NavigationMenuLink>
-                        </a>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <a href="#">
-                            <NavigationMenuLink className="flex items-center justify-center gap-2">
-                                Buy Crypto <SquareArrowUpRight />
-                            </NavigationMenuLink>
-                        </a>
-                    </NavigationMenuItem>
+            <div className="w-[75%] hidden lg:flex justify-between items-center">
+                <NavigationMenu>
+                    <NavigationMenuList className="flex gap-16 items-center">
+                        <NavigationMenuItem>
+                            <a href="#">
+                                <NavigationMenuLink>
+                                    Trade
+                                </NavigationMenuLink>
+                            </a>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger className="bg-none">Learn</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="flex flex-col p-4 md:w-[100px] lg:w-[200px] gap-4">
+                                    <li><a href="#" className="flex items-center text-center"><BookOpenText /> Learn Crypt</a></li>
+                                    <li><a href="#" className="flex items-center text-center"><Newspaper /> News</a></li>
+                                    <li><a href="#" className="flex items-center text-center"><Youtube /> Videos</a></li>
+                                    <li><a href="#" className="flex items-center text-center"><Mic />Podcast</a></li>
+                                    <li><a href="#" className="flex items-center text-center"><Mail /> Newsletter</a></li>
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <a href="#">
+                                <NavigationMenuLink className="flex items-center justify-center gap-2">
+                                    Earn
+                                </NavigationMenuLink>
+                            </a>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <a href="#">
+                                <NavigationMenuLink className="flex items-center justify-center gap-2">
+                                    Buy Crypto <SquareArrowUpRight />
+                                </NavigationMenuLink>
+                            </a>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
 
-                </NavigationMenuList>
-            </NavigationMenu>
-
-            <div className="flex items-center gap-4">
-                <Button className="bg-white hover:bg-slate-200 text-black"><CurrencyEth className="h-8 w-8" size={0} />Ethereum</Button>
-                <Button className="bg-secondary hover:bg-light_blue rounded-lg"><Wallet className="h-6 w-6 mr-2" />Connect wallet</Button>
-                <Button className="bg-white hover:bg-slate-200 text-black"><Settings /></Button>
+                <div className="flex items-center gap-4">
+                    <Button className="bg-white hover:bg-slate-200 text-black"><CurrencyEth className="h-8 w-8" size={0} />Ethereum</Button>
+                    <Button className="bg-secondary hover:bg-light_blue rounded-lg"><Wallet className="h-6 w-6 mr-2" />Connect wallet</Button>
+                    <Button className="bg-white hover:bg-slate-200 text-black"><Settings /></Button>
+                </div>
             </div>
+
+            <Sheet>
+                <SheetTrigger asChild className='flex lg:hidden'>
+                    <Button variant="outline" className='bg-transparent border-none text-white'><Menu /></Button>
+                </SheetTrigger>
+                <SheetContent className='flex lg:hidden justify-center'>
+                    <div className="w-full flex lg:hidden flex-col justify-center items-center">
+                        <NavigationMenu>
+                            <NavigationMenuList className="flex flex-col gap-16 items-center">
+                                <NavigationMenuItem>
+                                    <a href="#">
+                                        <SheetClose asChild>
+                                            <NavigationMenuLink>
+                                                Trade
+                                            </NavigationMenuLink>
+                                        </SheetClose>
+                                    </a>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger className="bg-none p-0">Learn</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <ul className="flex flex-col p-4 md:w-[100px] lg:w-[200px] gap-4">
+                                            <li>
+                                                <SheetClose asChild>
+                                                    <a href="#" className="flex items-center text-center"><BookOpenText /> Learn Crypt</a>
+                                                </SheetClose>
+                                            </li>
+                                            <li>
+                                                <SheetClose asChild>
+                                                    <a href="#" className="flex items-center text-center"><Newspaper /> News</a>
+                                                </SheetClose>
+                                            </li>
+                                            <li>
+                                                <SheetClose asChild>
+                                                    <a href="#" className="flex items-center text-center"><Youtube /> Videos</a>
+                                                </SheetClose>
+                                            </li>
+                                            <li>
+                                                <SheetClose asChild>
+                                                    <a href="#" className="flex items-center text-center"><Mic />Podcast</a>
+                                                </SheetClose>
+                                            </li>
+                                            <li>
+                                                <SheetClose asChild>
+                                                    <a href="#" className="flex items-center text-center"><Mail /> Newsletter</a>
+                                                </SheetClose>
+                                            </li>
+                                        </ul>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <a href="#">
+                                        <SheetClose asChild>
+                                            <NavigationMenuLink className="flex items-center justify-center gap-2">
+                                                Earn
+                                            </NavigationMenuLink>
+                                        </SheetClose>
+                                    </a>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <a href="#">
+                                        <SheetClose asChild>
+                                            <NavigationMenuLink className="flex items-center justify-center gap-2">
+                                                Buy Crypto <SquareArrowUpRight />
+                                            </NavigationMenuLink>
+                                        </SheetClose>
+                                    </a>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+
+                        <div className="flex flex-col items-center justify-center gap-4">
+                            <SheetClose asChild>
+                                <Button className="bg-white hover:bg-slate-200 text-black"><CurrencyEth className="h-8 w-8" size={0} />Ethereum</Button>
+                            </SheetClose>
+                            <SheetClose asChild>
+                                <Button className="bg-secondary hover:bg-light_blue rounded-lg"><Wallet className="h-6 w-6 mr-2" />Connect wallet</Button>
+                            </SheetClose>
+                            <SheetClose asChild>
+                                <Button className="bg-white hover:bg-slate-200 text-black"><Settings /></Button>
+                            </SheetClose>
+                        </div>
+                    </div>
+
+                </SheetContent>
+            </Sheet>
         </nav>
     )
 }
--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-none px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+  "group inline-flex h-auto w-max items-center justify-center rounded-md bg-none px-0 py-0 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
 )
 
 const NavigationMenuTrigger = React.forwardRef<
@@ -83,7 +83,7 @@ const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-  <div className={cn("absolute left-0 top-full flex justify-center")}>
+  <div className={cn("absolute left-0 top-full max-sm:top-[45%] flex justify-center")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
         "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-slate-200 bg-white text-slate-950 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)] dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
