a89214ed389bc7e304adbeafd4ed4baa9bf186d8
carloscurcino <carlosdanielf541@gmail.com>
2024-03-18 18:43:39 -0300

    Feat: doing Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className='w-full text-center p-4 border-t border-gray-500'>
+            <p>Developed by <a href="https://carloscurcino.com" target='_blank'>@carloscurcino</a></p>
+        </footer>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Navbar } from './components/navbar.tsx'
+import { Footer } from './components/footer.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Navbar />
-    <App />
+    <div className="flex h-screen flex-col">
+      <Navbar />
+      <div className="flex-1">
+        <App />
+      </div>
+      <Footer />
+    </div>
   </React.StrictMode>,
 )
