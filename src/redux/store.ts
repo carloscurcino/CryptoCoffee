63e4acd7f7db26ac126d00f5438934f346cdcffb
carloscurcino <carlosdanielf541@gmail.com>
2024-03-21 23:34:10 -0300

    Fix: serializable error

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -19,6 +19,9 @@ const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
